472775378da1f0e738c6da38465e193c7d993b20
Ali T. Heidari <theidari.ali@gmail.com>
2023-04-04 18:25:09 -0400

    Update helpers.py

--- src/package/helpers.py
+++ src/package/helpers.py
@@ -6,6 +6,7 @@
 from sklearn.model_selection import train_test_split # to get training and test sets
 from sklearn.preprocessing import StandardScaler # to removes the mean and scales each feature/variable to unit variance
 
+!pip install keras-tuner
 import pandas as pd
 import keras_tuner
 import tensorflow as tf
